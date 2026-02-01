d0f95d8634e8238a287223d3352efd4114fc0074
Lingala Maheswar Reddy <134993286+LINGALA-MAHESWAR-REDDY@users.noreply.github.com>
2026-02-01 20:38:08 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,12 @@ public class HelloWorld {
     System.out.println("this is madhu");
     System.out.println("This is a new Line");
 
+     System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("This is a new Line");
+
+     System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("This is a new Line");
   }
 }
